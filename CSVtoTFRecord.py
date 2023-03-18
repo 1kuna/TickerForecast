afb5e3b86477abc723968afcd1e79a06ffe949b6
asikuna <zcanepa19@gmail.com>
2023-03-18 02:48:22 -0400

    Update to the CSV -> TFRecord script

--- CSVtoTFRecord.py
+++ CSVtoTFRecord.py
@@ -15,38 +15,44 @@ def get_file_path(*subdirs, filename=None):
 input_file = get_file_path('intraday', filename=f'TRAIN_COMBINED.csv')
 output_file = get_file_path(f'intraday', filename=f'TRAIN_COMBINED.tfrecord')
 
-# Read Parquet file into a Pandas DataFrame
-df = pd.read_csv(input_file)
+data = pd.read_csv(input_file)
 
-# Set 'time' column as index
-df.set_index('time', inplace=True)
+# Define the feature dictionary
+feature_dict = {
+    'time': tf.float32,
+    'open': tf.float32,
+    'high': tf.float32,
+    'low': tf.float32,
+    'close': tf.float32,
+    'volume': tf.float32,
+    'sma50': tf.float32,
+    'sma200': tf.float32,
+    'ema8': tf.float32,
+    'ema20': tf.float32,
+    'rsi': tf.float32,
+    'macd': tf.float32,
+    'stoch': tf.float32,
+    'vwap': tf.float32,
+    'aroon_up': tf.float32,
+    'aroon_down': tf.float32,
+    'roc': tf.float32,
+    'obv': tf.float32,
+    'adi': tf.float32
+}
 
-# Get 'open' column as target variable
-target = df.pop('open')
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
-# Convert Pandas DataFrame to TensorFlow Example
-def df_to_example(row):
-    # Convert each row to a TensorFlow FeatureDict
-    features = {
-        col: tf.train.Feature(float_list=tf.train.FloatList(value=[val]))
-        for col, val in row.items()
-    }
-    # Wrap the FeatureDict in a TensorFlow Example
-    example = tf.train.Example(features=tf.train.Features(feature=features))
-    return example
+# Function to convert a row in the CSV to a tf.train.Example
+def csv_row_to_example(row):
+    features = {}
+    for col, dtype in feature_dict.items():
+        if dtype == tf.float32:
+            features[col] = _float_feature(row[col])
+    return tf.train.Example(features=tf.train.Features(feature=features))
 
-# Write each row of the DataFrame as a serialized Example to the TFRecord file
+# Write the TFRecord file
 with tf.io.TFRecordWriter(output_file) as writer:
-    for index, row in df.iterrows():
-        example = df_to_example(row)
-        writer.write(example.SerializeToString())
-
-# Write target variable to separate TFRecord file
-with tf.io.TFRecordWriter(output_file.split('.')[0] + '_target.tfrecord') as writer:
-    for val in target:
-        example = tf.train.Example(features=tf.train.Features(
-            feature={
-                'target': tf.train.Feature(float_list=tf.train.FloatList(value=[val]))
-            }
-        ))
+    for _, row in data.iterrows():
+        example = csv_row_to_example(row)
         writer.write(example.SerializeToString())
